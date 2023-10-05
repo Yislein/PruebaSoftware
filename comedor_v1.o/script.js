df38457806e4d1d48f3668cd51e66eff0f4b4a8d
JonasMart369 <144301144+JonasMart369@users.noreply.github.com>
2023-10-05 09:21:13 -0600

    Add files via upload

---
+++ comedor_v1.o/script.js
@@ -0,0 +1,22 @@
+const menuPalanca =document.querySelector('.menupalanca');
+const navegacion=document.querySelector('.navegacion');
+
+menuPalanca.onclick=function()
+{
+    navegacion.classList.toggle('open');
+}
+
+const lista=document.querySelectorAll('.list');
+
+function activarlink()
+{
+    lista.forEach((item)=>
+    item.classList.remove('active'))
+    this.classList.add('active')
+}
+lista.forEach((item)=>
+item.addEventListener('click',activarlink)
+)
+
+
+
